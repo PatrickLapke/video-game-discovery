8a7ab61be9a714435af21fa7e88fa4532f91e06d
Patrick Lapke <plapke@zagmail.gonzaga.edu>
2023-11-18 18:18:03 +0000

    Added a dynamic alert and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  const [alert, setAlert] = useState(false);
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
+      {alert == true && (
+        <Alert children="Alert Here" onClose={() => setAlert(false)}></Alert>
+      )}
+      <Button color="primary" onClick={() => setAlert(true)}>
+        {"My Button"}
+      </Button>
     </div>
   );
 }
